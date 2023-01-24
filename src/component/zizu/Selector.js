660caf6e2bc2e1552e2b6c8a14e902e38bb4b39d
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2023-01-24 19:02:44 +0300

    created components and added structure to them

---
+++ src/component/zizu/Selector.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Selector(){
+    return(
+        <div>
+            <p>Carbon Facts </p>
+            <h2>Lets Calculate your Emissions </h2>
+            <h3>Choose one of the following</h3>
+            <div>
+                <ul>
+                    <li> Electricy</li>
+                    <li> Fuel</li>
+                    <li> Shipping</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Selector;
