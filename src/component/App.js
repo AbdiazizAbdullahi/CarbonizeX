d2e4a9776632317d115e5ced20c342de69e43f75
ntwigamartin <ntwiga.martin@gmail.com>
2023-01-25 15:43:37 +0300

    shipping component

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,13 @@
 import Stima from './zizu/Electricity';
 import Selector from './zizu/Selector';
+import Shipping from './zizu/Shipping';
 
 function App() {
   return (
     <div className="App">
       <Selector />
       <Stima/>
+      <Shipping/>
     </div>
   );
 }
