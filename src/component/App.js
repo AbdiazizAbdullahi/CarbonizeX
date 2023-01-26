b811cfc7a8fe43ead1a204c4e0f12bc04bbd7b04
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2023-01-26 10:10:34 +0300

    updated App.js

--- src/component/App.js
+++ src/component/App.js
@@ -1,17 +1,30 @@
-import Stima from './zizu/Electricity';
-import Fuel from './zizu/Fuel';
-import Selector from './zizu/Selector';
-import Shipping from './zizu/Shipping';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Stima from './component/zizu/Electricity';
+import Fuel from './component/zizu/Fuel';
+import Selector from './component/zizu/Selector';
+import './App.css';
+import Navbar from './component/HomePage/NavBar';
+import Shipping from './component/zizu/Shipping';
+import Login from './component/HomePage/Login';
+import Home from './component/HomePage/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Selector />
-      <Stima/>
-       <Fuel />
-      <Shipping/>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/stima" ><Stima/></Route>
+        <Route path="/fuel" ><Fuel/></Route>
+        <Route path="/shipping" ><Shipping/></Route>
+        <Route path="/login" ><Login/></Route>
+        <Route path="/estimate" ><Selector/></Route>
+        <Route exact path="/" ><Home/></Route>
+      </Routes>  
+    </BrowserRouter>
   );
 }
 
 export default App;
+
