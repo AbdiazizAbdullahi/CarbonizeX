a1da4acd550d615d2e8b23c711b0eb4fb45301fd
regular-meet-8 <eddiekim444@gmail.com>
2023-01-26 12:48:34 +0300

    table.js

---
+++ src/component/MaterialTable.js
@@ -0,0 +1,16 @@
+import MaterialTable from "material-table";
+
+
+export const Table = () => {
+    const data = [
+      { country: "", grams: "", kgs: "", pounds: "" },
+      { country: "", grams: "", kgs: "", pounds: "" },
+      { country: "", grams: "", kgs: "", pounds: "" },
+      { country: "", grams: "", kgs: "", pounds: "" },
+      { country: "", grams: "", kgs: "", pounds: "" },
+      { country: "", grams: "", kgs: "", pounds: "" },
+    ];
+  };
+
+
+  
