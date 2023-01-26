6f367e068498dd220abd4efa71970798180227d3
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2023-01-26 10:04:23 +0300

    created Navbar.js,Home.js and Login.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Stima from './component/zizu/Electricity';
-import Fuel from './component/zizu/Fuel';
-import Selector from './component/zizu/Selector';
-import './App.css';
-import Navbar from './component/HomePage/NavBar';
-import Shipping from './component/zizu/Shipping';
-import Login from './component/HomePage/Login';
-import Home from './component/HomePage/Home';
+import Stima from './zizu/Electricity';
+import Fuel from './zizu/Fuel';
+import Selector from './zizu/Selector';
+import Shipping from './zizu/Shipping';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar/>
-
-      <Routes>
-        <Route path="/stima" ><Stima/></Route>
-        <Route path="/fuel" ><Fuel/></Route>
-        <Route path="/shipping" ><Shipping/></Route>
-        <Route path="/login" ><Login/></Route>
-        <Route path="/estimate" ><Selector/></Route>
-        <Route exact path="/" ><Home/></Route>
-      </Routes>  
-    </BrowserRouter>
+    <div className="App">
+      <Selector />
+      <Stima/>
+       <Fuel />
+      <Shipping/>
+    </div>
   );
 }
 
 export default App;
-
--- src/component/zizu/Shipping.js
+++ src/component/zizu/Shipping.js
@@ -1,18 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Shipping(){
-    return(
-        <div>
-            <div>
-                <h4>Enter details of the shipped item</h4>
-                <form>
-                    <input type="number" />
-                    <input type="number" />
-                    <button>Estimate</button>
-                </form>
-            </div>
-        </div>
-    )
-}
+const Shipping = () => {
+    const [response, setResponse] = useState({});
+    const [weight, setWeight] = useState('');
+    const [distance, setDistance] = useState('');
+    
+    
+    
+    const handleWeightChange = (event) => {
+      setWeight(event.target.value);
+    }
+
+    const handleDistanceChange = (event) => {
+        setDistance(event.target.value);
+      }
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      const requestBody = {
+        type: "shipping",
+        weight_value: ((weight) * 1000),
+        weight_unit: "g",
+        distance_value: (distance),
+        distance_unit: "km",
+        transport_method: "truck"
+      };
+  
+      try {
+        const res = await fetch('https://www.carboninterface.com/api/v1/estimates', {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer v7qsKOiNCcIiEUooI5cmTA`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(requestBody),
+        });
+        const data = await res.json();
+        setResponse(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  
+  
+      return(
+          <div>
+              <div>
+                  <h4>Enter the Shipping Units to estimate</h4>
+                  <form onSubmit={handleSubmit}>
+                      <input type="number" value={weight} onChange={handleWeightChange} placeholder="weight" />
+                      <input type="number" value={distance} onChange={handleDistanceChange} placeholder="distance" />
+                      <button type="submit">Estimate</button>
+                  </form>
+              </div>
+  
+              {response.data && response.data.attributes && <p>Carbon Emission: {response.data.attributes.carbon_kg} kg</p>}
+              
+          </div>
+      )
+  }
 
 export default Shipping;
